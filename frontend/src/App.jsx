45b91479f362ddce09f53e754a753db6c61fbcb8
Om Juvatkar <omjuvatkar123@gmail.com>
2025-12-15 22:53:54 +0530

    info: Added routes to project

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,23 @@
 import './App.css'
+import {BrowserRouter, Routes, Route} from "react-router-dom"
+
+import {Signup} from "./pages/Signup"
+import {Signin} from "./pages/Signin"
+import {Dashboard} from "./pages/Dashboard"
+import {Send} from "./pages/Send"
 
 function App() {
 
   return (
     <>
-      <p>Hello World</p>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/signin" element={<Signin />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/send" element={<Send />} />
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
---
+++ frontend/src/pages/Signup.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const Signup = () => {
+  return (
+    <div className="text-4xl font-bold text-blue-500">Signup</div>
+  )
+}
+
+export default Signup
