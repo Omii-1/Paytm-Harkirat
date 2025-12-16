9304ada4b67f07fedaa2f90cb7b26bd1e97e291f
Om Juvatkar <omjuvatkar123@gmail.com>
2025-12-17 00:28:21 +0530

    feat: created inner components for signup and signin page

--- frontend/src/pages/Signup.jsx
+++ frontend/src/pages/Signup.jsx
@@ -1,9 +1,22 @@
 import React from 'react'
 
-export const Signup = () => {
+import { Heading } from '../components/Heading'
+import { SubHeading } from "../components/SubHeading"
+import { InputBox } from "../components/InputBox"
+import { Button } from '../components/Button'
+import { BottomWarning } from '../components/BottomWarning'
+
+export function Signup () {
   return (
-    <div className="text-4xl font-bold text-blue-500">Signup</div>
+    <>
+      <Heading label={"Sign up"} />
+      <SubHeading label={"Enter your information to create an account"} />
+      <InputBox label={"First Name"} placeholder={"John"} />
+      <InputBox label={"Last Name"} placeholder={"Doe"} />
+      <InputBox label={"Email"} placeholder={"johndoe@gmail.com"} />
+      <InputBox label={"Password"} placeholder={"12345"} />
+      <Button label={"Sign up"} onClick={()=>{}} />
+      <BottomWarning label={"Already have an account?"} to={"/signin"} buttonText={"Sign in"}/>
+    </>
   )
 }
-
-export default Signup
