9522e94a41ee555aba245c575dd3b43ae2f2db83
Om Juvatkar <omjuvatkar123@gmail.com>
2025-12-18 00:01:58 +0530

    feat: created Dashboard and send page UI

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,8 +1,16 @@
 import React from 'react'
 
+import { AppBar } from '../components/AppBar'
+import { Balance } from '../components/Balance'
+import { Users } from '../components/Users'
+
 export const Dashboard = () => {
   return (
-    <div>Dashboard</div>
+    <div>
+      <AppBar />
+      <Balance value={"3000"} />
+      <Users />
+    </div>
   )
 }
 
