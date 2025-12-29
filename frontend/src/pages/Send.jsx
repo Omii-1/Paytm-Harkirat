ec141e33b1097d0f5358e975a95682e578098237
Om Juvatkar <omjuvatkar123@gmail.com>
2025-12-29 23:55:52 +0530

    feat: integrated send money

--- frontend/src/pages/Send.jsx
+++ frontend/src/pages/Send.jsx
@@ -1,6 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
+import { useSearchParams, useNavigate } from "react-router-dom"; 
+import axios from "axios";
 
 export const Send = () => {
+
+  const [searchParams] = useSearchParams()
+
+  const id = searchParams.get("id")
+  const name = searchParams.get("name")
+  const [amount, setAmount] = useState()
+  const navigate = useNavigate()
+
+  const handleChange = (e) => {
+    setAmount(e.target.value)
+  }
+
+  const handleClick = async () => {
+    const res = await axios.post("http://localhost:3000/api/v1/account/transfer", {
+      to: id,
+      amount: amount
+    }, {
+      headers: {
+        Authorization: localStorage.getItem("token")
+      }
+    })
+
+    console.log(res.data);
+    
+    navigate("/dashboard")
+
+  }
+
+
   return (
     <div class="flex justify-center h-screen bg-gray-100">
       <div className="h-full flex flex-col justify-center">
@@ -11,9 +42,9 @@ export const Send = () => {
           <div class="p-6">
             <div class="flex items-center space-x-4">
               <div class="w-12 h-12 rounded-full bg-green-500 flex items-center justify-center">
-                <span class="text-2xl text-white">A</span>
+                <span class="text-2xl text-white">{name[0].toUpperCase()}</span>
               </div>
-              <h3 class="text-2xl font-semibold">Friend's Name</h3>
+              <h3 class="text-2xl font-semibold">{name}</h3>
             </div>
             <div class="space-y-4">
               <div class="space-y-2">
@@ -28,9 +59,11 @@ export const Send = () => {
                   class="flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-sm"
                   id="amount"
                   placeholder="Enter amount"
+                  value={amount}
+                  onChange={handleChange}
                 />
               </div>
-              <button class="justify-center rounded-md text-sm font-medium ring-offset-background transition-all duration-300 h-10 px-4 py-2 w-full bg-green-500 text-white cursor-pointer hover:bg-green-600 hover:shadow-lg hover:scale-101">
+              <button class="justify-center rounded-md text-sm font-medium ring-offset-background transition-all duration-300 h-10 px-4 py-2 w-full bg-green-500 text-white cursor-pointer hover:bg-green-600 hover:shadow-lg hover:scale-101" onClick={handleClick}>
                 Initiate Transfer
               </button>
             </div>
